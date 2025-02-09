97339aa824baec727043180d278650f6d1eded24
johary <joharyvonimbola@gmail.com>
2025-02-09 19:29:15 +0300

    Projet terminee

--- conception/sql/06-data.sql
+++ conception/sql/06-data.sql
@@ -12,3 +12,6 @@ INSERT INTO type_reference (id_type, type) VALUES
 INSERT INTO reference (id_reference, duree, id_type) VALUES
 (DEFAULT, 90, (SELECT id_type FROM type_reference WHERE type = 'pin')),
 (DEFAULT, 900, (SELECT id_type FROM type_reference WHERE type = 'token'));
+
+
+INSERT INTO utilisateur (id_utilisateur, email, mdp, tentative_max, id_etat) VALUES (DEFAULT, 'mikajyjoharytsiorysarobidy@gmail.com', '00d70c561892a94980befd12a400e26aeb4b8599', 3, 2);
